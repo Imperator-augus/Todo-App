f3f748d1178d339e6e5db083120be2b18c973fd4
CHAN YE <qjdrptmzk@naver.com>
2022-07-12 16:29:11 +0900

    중간마무리

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-    <div id="app">
-      <TodoHeader></TodoHeader>
-      <TodoInput @childAddTodo="addTodo" @modal="showModal"></TodoInput>
-      <TodoList @childRemoveTodo="removeTodo" v-bind:propsItems="todoItems"></TodoList>
-      <TodoFooter @clearTodo="clearTodo"></TodoFooter>
-    </div>
-    <AlertModal @modalClose="closeModal" @close="closeModal" :show="modalShow" header="알림창" body="내용을 입력해 주세요." ></AlertModal>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput @childAddTodo="addTodo" @noInputValue="showModal"></TodoInput>
+    <TodoList :propsItems="todoItems" @childRemoveTodo="removeTodo"></TodoList>
+    <TodoFooter @clearTodo="clearTodo"></TodoFooter>    
+  </div>
+  <AlertModal :show="modalShow" header="알림창" body="내용을 입력해 주세요." @close="hiddenModal"></AlertModal>
 </template>
 
 <script>
@@ -14,46 +14,61 @@ import TodoInput from './components/todo/TodoInput.vue';
 import TodoList from './components/todo/TodoList.vue';
 import TodoFooter from './components/todo/TodoFooter.vue';
 import AlertModal from './components/common/AlertModal.vue';
+//import TodoDao from './dao/TodoDao';
+import axios from 'axios';
+import DateUtils from './utils/DateUtils';
 
 export default {
   name: 'App',
   data() {
     return {
-      todoItems: [],
-      cnt: 0,
+      todoItems: [],      
       modalShow: false,
     }
   },
   methods: {
-    addTodo(todoItem) {
-      this.todoItems.push({
-        key: this.cnt++,
-        value: todoItem,
-      });
-    },
-    removeTodo(key) {
-      // this.todoItems.splice(idx, 1);
-      this.todoItems.forEach((item, idx) => {
-        if(item.key === key) {
-          this.todoItems.splice(idx, 1);
-        }
-      })
-    },
-    clearTodo(){
-      this.todoItems.splice(0);
-      this.cnt = 0;
-    },
-    changeValue() {
-      const json = JSON.stringify(this.todoItems);
-      localStorage.setItem('todoItems', json);
-      localStorage.setItem('cnt', this.cnt);
-    },
-    closeModal() {
-      this.modalShow = false;
-    },
     showModal() {
       this.modalShow = true;
-    } 
+    },
+    hiddenModal() {
+      this.modalShow = false;
+    },
+    addTodo(todoItem) {
+      const param = {
+        'todo': todoItem
+      };
+      axios.post('/todo/index', param)
+      .then(res => {
+        console.log(res);
+        if(res.status === 200 && res.data) {
+          const item = {
+            'itodo': res.data.result,
+            'todo': todoItem,
+            'created_at': DateUtils.getTimestamp(new Date())
+          }
+          this.todoItems.push(item);
+        }
+      });    
+    }, 
+    removeTodo(key) {      
+      this.todoItems.forEach((item, idx) => {        
+        if(item.itodo === key) {
+          this.todoItems.splice(idx, 1);
+          axios.delete(`/todo/index/${item.itodo}`)
+          .then(res => {
+            console.log(res);
+          });
+        }
+      });
+    },
+    clearTodo() {
+      axios.delete(`/todo/index`)
+      .then(res => {
+        if(res.status === 200 && res.data.result) {
+          this.todoItems.splice(0);
+        }
+      });
+    }
   },
   components: {
     TodoHeader,
@@ -61,32 +76,32 @@ export default {
     TodoList,
     TodoFooter,
     AlertModal
-  },
-  watch: {
-    todoItems: {
-      deep: true,//todoItems배열안의 값이 변하는걸 감지 (레퍼런스타입) .이두개 /주소값이 바뀌는걸 감지하는건 false 문자,숫자 (프라모티브타입) .이 한개/
-      handler() {
-        this.changeValue();
-      }
-    }
-  },
-  created() {
-    const json = localStorage.getItem("todoItems");
-    if(json) {
-      const todoItems = JSON.parse(json);
-      todoItems.forEach(item => {
-        this.todoItems.push(item);
-      });
-      const cnt = localStorage.getItem("cnt");
-      this.cnt = cnt;
-    }
-  }
+  }, 
+  created () {  
+   axios.get('/todo/index')
+   .then(res => {
+     if(res.status === 200 && res.data.length > 0) {
+       res.data.forEach(item => {
+         this.todoItems.push(item);
+       });
+     }
+     console.log(res);
+   });  
+  }  
 }
 </script>
 
 <style>
-body { text-align: center; background-color: #f6f6f8;}
-input { border-style: groove; width: 200px;}
-button { border-style: groove;}
+body { text-align: center; background-color: #F6F6F8; }
+input { border-style: groove; width: 200px; }
+button { border-style: groove; }
+
+.ctnt { font-size: 1rem; }
+.d-flex { display: flex; }
+
+.flex-col { flex-direction: column; }
+.flex-row { flex-direction: row; }
+.grow_1 { flex-grow: 1; }
 .shadow { box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03); }
+.justify_content_evenly { justify-content: space-evenly; }
 </style>
